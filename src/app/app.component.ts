580b26d9ff233db26f8175c71ae8a859b6653089
Diego Veras <verasdiegoca78@gmail.com>
2024-12-23 20:57:51 -0300

    add environment configuration, global styles, and integrate PrimeNG and Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { FloatLabelModule } from "primeng/floatlabel"
+import { InputTextModule } from 'primeng/inputtext';
+import { FormsModule } from '@angular/forms';
+import { StyleClassModule } from 'primeng/styleclass';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,
+    MatSlideToggleModule,
+    FloatLabelModule, InputTextModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'login';
+  value: string | undefined;
 }
