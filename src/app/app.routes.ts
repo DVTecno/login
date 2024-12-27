a3ee25807ca7cf2271e89386e71d3ae2eef59fda
Diego Veras <verasdiegoca78@gmail.com>
2024-12-27 17:58:36 -0300

    Add lazy loading for authentication and dashboard routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'auth',
+    loadChildren: ()=> import('./auth/features/shell/auth.routes'),
   },
   {
     path: 'dashboard',
+    loadComponent: ()=> import('./dashboard/dashboard.component'),
   }
 ];
