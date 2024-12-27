8968dc38c76ec3a0ccc4e93a7204606853ce6df1
Diego Veras <verasdiegoca78@gmail.com>
2024-12-27 17:54:10 -0300

    Add log-in and sign-up components with basic structure and tests

---
+++ src/app/auth/features/log-in/log-in.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-log-in',
+  imports: [],
+  templateUrl: './log-in.component.html',
+  styles: ``
+})
+export default class LogInComponent {
+
+}
