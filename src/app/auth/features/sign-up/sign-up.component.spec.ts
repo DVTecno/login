13784b24be827edfed55cfd5981555bb559c61c6
Diego Veras <verasdiegoca78@gmail.com>
2024-12-27 16:52:49 -0300

    Refactor component imports to use default import syntax in spec files

--- src/app/auth/features/sign-up/sign-up.component.spec.ts
+++ src/app/auth/features/sign-up/sign-up.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { SignUpComponent } from './sign-up.component';
-
-
+import SignUpComponent from './sign-up.component';
 
 describe('SignUpComponent', () => {
   let component: SignUpComponent;
