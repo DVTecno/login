33bc92bba213c8d0cb70547d338e93139020e697
Diego Veras <verasdiegoca78@gmail.com>
2024-12-27 21:11:52 -0300

    Implement reactive form for sign-up component with email and password validation

--- src/app/auth/features/sign-up/sign-up.component.ts
+++ src/app/auth/features/sign-up/sign-up.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+interface LoginForm {
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
 
 @Component({
   selector: 'app-sign-up',
+  standalone: true,
   imports: [RouterLink],
   templateUrl: './sign-up.component.html',
   styles: ``
 })
 export default class SignUpComponent {
+  form;
 
+  constructor(private _formBuilder: FormBuilder) {
+    this.form = this._formBuilder.group<LoginForm>({
+      email: this._formBuilder.nonNullable.control('', [Validators.required]),
+      password: this._formBuilder.nonNullable.control('', [Validators.required]),
+    });
+  }
 }
