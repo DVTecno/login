6c29f997df33d21241a84a920be5501b6dd3c06d
Diego Veras <verasdiegoca78@gmail.com>
2025-01-02 15:29:02 -0300

    Add StorageService for token management and update signUp method in AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,17 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { tap} from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
+import { StorageService } from '../../shared/data-access/storege.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private _http: HttpClient) {}
+  constructor(private _http: HttpClient, private _storage: StorageService) {}
 
-  signUp(email: string, password: string): Observable<void> {
+  signUp(email: string, password: string): Observable<{ token: string, refreshToken: string }> {
     // Implement your signup logic here
-    return this._http.post<void>(`${environment.API_URL}/api/auth/signup`, { email, password });
+    return this._http.post<{ token: string, refreshToken: string }>(`${environment.API_URL}/api/auth/signup`, { email, password })
+    .pipe(tap((response) => {
+      this._storage.set('token', response.token);
+      this._storage.set('refreshToken', response.refreshToken);
+    }));
   }
 
   logIn(email: string, password: string): Observable<void> {
